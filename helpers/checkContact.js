0f00e8456072b9e7fe2ada91ed1062a76aaaf105
Dmytro Riabtsun <riabtsun@i.ua>
2024-12-03 22:59:54 +0200

    slice phone number

--- helpers/checkContact.js
+++ helpers/checkContact.js
@@ -5,8 +5,9 @@ const mainMenu = require("../keyboards/mainMenu");
 const checkContact = new Composer();
 
 checkContact.on("message:contact", async (ctx) => {
-  console.log(`phoneNumber before is: ${ctx.message.contact}`);
-  const phoneNumber = `${ctx.message.contact.phone_number}`;
+  const phoneNumber = ctx.message.contact.phone_number.startsWith("+")
+    ? ctx.message.contact.phone_number.slice(3)
+    : ctx.message.contact.phone_number.slice(2);
   const telegramId = ctx.from.id;
   console.log("phoneNumber is: " + phoneNumber);
 
